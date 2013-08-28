7fc6ef2ca60464c86db0b19c3d949e598428d005
Zach Goldberg <zach@zachgoldberg.com>
2013-08-28 15:03:34 -0700

    Add setup, move readme

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+long_description = open('README.md').read()
+
+setup(name="python-angellist",
+      version="1.0.0",
+      py_modules=["angellist"],
+      description="Libraries for interacting with the Angellist API",
+      author="Falicon and Zach Goldberg",
+      author_email="zach@zachgoldberg.com, falicon@github",
+      license="WTFPL",
+      url="http://github.com/zachgoldberg/AngelList",
+      long_description=long_description,
+      platforms=["any"],
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Intended Audience :: Developers",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development" +
+                   " :: Libraries :: Python Modules",
+                   ],
+      )
